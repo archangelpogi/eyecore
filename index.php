cfe541a1b1bb2c7e514a38a2118ee3b300e261d1
Kharlaashanti123 <rivera.kharlaashanti@ncst.edu.ph>
2026-07-23 16:40:58 +0800

    Edit index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // TEST SFTP
+// RHAINE RIVERA
 // Check if config file exists
 if (file_exists('includes/config.php')) {
     include 'includes/config.php';
@@ -1072,7 +1073,7 @@ $clinic_icon_colors = ['#059669','#1d4ed8','#9d174d','#5b21b6','#92400e','#0f766
                     <a href="#clinics" class="nav-link">Clinics</a>
                     <a href="#contact" class="nav-link">Contact</a>
                     <a href="auth/register.php" class="nav-link nav-link-highlight">Partner With Us</a>
-                    <a href="suppliers/supplier_login.php?register=1" class="nav-link">Supplier</a>
+                    <a href="suppliers/supplier_login.php?register=1" class="nav-link">Suppliersss</a>
                 </div>
             </div>
             <div class="nav-right">
